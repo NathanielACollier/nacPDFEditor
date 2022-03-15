b596b4988ae4022ed701de71a0a1b39ea0ed104c
Nathaniel A Collier <drxs3v3n@gmail.com>
2022-03-15 17:27:37 -0500

    Previous and next buttons are in place and working

--- nacPDFEditor/repos/MainWindow.cs
+++ nacPDFEditor/repos/MainWindow.cs
@@ -36,18 +36,34 @@ public static class MainWindow
                             return 0;
                         }, style: new Style {width = 50})
                         .TextFor(nameof(model.PageCountDisplayText), style:new Style{width = 50})
-                        .Button("Prev", (args) =>
-                        {
-
-                        }, style:new Style{width = 50})
-                        .Button("Next", (args) =>
-                        {
-
-                        }, style:new Style{width = 50});
+                        .Button("Prev", onClick: onclick_prevPDFPageButton, style:new Style{width = 50})
+                        .Button("Next", onClick: onclick_nextPDFPageButton, style:new Style{width = 50});
                 }, style: new Style{height = 30})
             .Image(nameof(model.CurrentPageImage));
         }, style: new Style{isVisibleModelName = nameof(model.IsPDFReady)})
         .Display();
+    }
+
+    private static void onclick_nextPDFPageButton(object obj)
+    {
+        if (model.currentPageNumber > pdfImageReader.PageCount - 2)
+        {
+            return;
+        }
+
+        model.currentPageNumber++;
+        model.CurrentPageImage = pdfImageReader.getPageAsImage(model.currentPageNumber);
+    }
+
+    private static void onclick_prevPDFPageButton(object obj)
+    {
+        if (model.currentPageNumber < 1)
+        {
+            return;
+        }
+
+        model.currentPageNumber--;
+        model.CurrentPageImage = pdfImageReader.getPageAsImage(model.currentPageNumber);
     }
 
     private static void OnPDFFilePathChange(string newFilePath)
